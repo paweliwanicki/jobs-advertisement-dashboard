808910e175a71b8600a4d2e0558e9f8c4f7a0f46
paweliwanicki <pawel_iwanicki@outlook.com>
2023-09-12 22:29:13 +0200

    some fixes and clean up, add loading spinner, add enums (route paths, httpmethod), fix contexts

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,17 +4,20 @@ import { RouterProvider } from 'react-router-dom';
 import { useRouter } from './hooks/useRouter';
 import AuthProvider from './providers/AuthProvider';
 import { UserProvider } from './providers/UserProvider';
+import ThemeProvider from './providers/ThemeProvider';
 const { router } = useRouter();
 
 export function App() {
   return (
-    <AuthProvider>
-      <UserProvider>
-        <CookiesProvider>
-          <RouterProvider router={router} />
-        </CookiesProvider>
-      </UserProvider>
-    </AuthProvider>
+    <CookiesProvider>
+      <AuthProvider>
+        <UserProvider>
+          <ThemeProvider>
+            <RouterProvider router={router} />
+          </ThemeProvider>
+        </UserProvider>
+      </AuthProvider>
+    </CookiesProvider>
   );
 }
 
---
+++ apps/frontend/src/components/common/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,5 @@
+import spinner from './spinner.svg';
+
+export const LoadingSpinner = () => {
+  return <img src={spinner} alt="loading spinner" />;
+};
